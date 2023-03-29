096df402006227813a868f3384868de98f620493
fadega <fadega.araya@gmail.com>
2023-03-28 20:14:01 -0600

    (Feature to pull list of employees done)

--- src/main/java/com/techkoda/employeemgmtapi/controller/EmployeeController.java
+++ src/main/java/com/techkoda/employeemgmtapi/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.techkoda.employeemgmtapi.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 //allow this url
 //@CrossOrigin(origins = "http://locahost:3000")
 //allow everything
@@ -25,5 +27,11 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee){
 
         return employeeService.createEmployee(employee);
+    }
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+
+        return employeeService.getAllEmployees();
+
     }
 }
--- src/main/java/com/techkoda/employeemgmtapi/services/EmployeeService.java
+++ src/main/java/com/techkoda/employeemgmtapi/services/EmployeeService.java
@@ -2,6 +2,10 @@ package com.techkoda.employeemgmtapi.services;
 
 import com.techkoda.employeemgmtapi.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
 }
